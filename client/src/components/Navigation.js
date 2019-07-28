bb91793b04194ff69b983731e4f0fbf9b0dc6f39
Kyle Erdahl <kyle.m.erdahl@gmail.com>
2019-07-27 23:41:01 -0500

    Attempting to fix navigation

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
@@ -11,9 +12,24 @@ function Navigation() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
+            <li className="nav-item nav-link">
+              <Link to="/" className="white-link">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item nav-link">
+              <Link to="/forecast" className="white-link">
+                Forecast
+              </Link>
+            </li>
+            <li className="nav-item nav-link">
+              <Link to="/hourly" className="white-link">
+                Hourly Forecast
+              </Link>
+            </li>
+            {/* <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/forecast">Forecast</Nav.Link>
-            <Nav.Link href="/hourly">Hourly Forecast</Nav.Link>
+            <Nav.Link href="/hourly">Hourly Forecast</Nav.Link> */}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
